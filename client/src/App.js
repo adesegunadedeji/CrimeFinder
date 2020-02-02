73133ad2a9e31cf4d2871ae2c3bc6fc3d9f30c34
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-02-02 16:11:53 -0500

    Added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import CrimeSearch from './Components/Listings/CrimeList';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
 function App() {
   return (
     <div className="App">
-    <CrimeSearch/>
+      <Switch>
+      <Route path ="/home" component ={Home}>
+        </Route>
+        <Route path ="/crimes" component ={CrimeSearch}>
+        </Route>
+      </Switch>
+   
     </div>
   );
 }
